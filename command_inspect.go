5e704184df6838a87d3e53aaee8020d157cae591
Kevin Robles <kevinronu@gmail.com>
2023-10-12 23:25:42 -0500

    chore: add inspect command

---
+++ command_inspect.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func callbackInspect(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("No pokemon name provided")
+	}
+
+	pokemonName := args[0]
+
+	pokemon, ok := cfg.caughtPokemon[pokemonName]
+	if !ok {
+		return errors.New("You haven't caught this pokemon yet")
+	}
+
+	fmt.Printf("Name: %s\n", pokemon.Name)
+	fmt.Printf("Height: %d\n", pokemon.Height)
+	fmt.Printf("Height: %d\n", pokemon.Weight)
+	fmt.Println("Stats:")
+	for _, stat := range pokemon.Stats {
+		fmt.Printf(" - %s: %d\n", stat.Stat.Name, stat.BaseStat)
+	}
+	fmt.Println("Types:")
+	for _, typ := range pokemon.Types {
+		fmt.Printf(" - %s ", typ.Type.Name)
+	}
+
+	fmt.Println()
+
+	return nil
+}
